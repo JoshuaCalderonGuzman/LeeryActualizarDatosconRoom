7b822bc75ebb3f0e286f27fe28c5587d50664811
Joshua Calderon Guzman <s22120150@alumnos.itsur.edu.mx>
2025-10-14 21:40:11 -0600

    Arreglo de error

--- app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -144,7 +144,7 @@ private fun ItemDetailsBody(
             onClick = onSellItem,
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.small,
-            enabled = true
+            enabled = !itemDetailsUiState.outOfStock
         ) {
             Text(stringResource(R.string.sell))
         }
